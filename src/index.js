0a7da63f9b57103bb955c3f763a0c7e80e751d45
theCalculatar <alpheusm13@gmail.com>
2025-01-07 11:01:57 +0200

    overload methods to allow express() to work with default configuration

--- src/index.js
+++ src/index.js
@@ -18,21 +18,35 @@ const init = (express, options) => {
   //custom app func that accepts meta data
   methods.forEach((method) => {
     const original = app[method]
-    app[method] = (path, metadata = {}, ...handlers) => {
-      if (path.startsWith('/')) {
-        routes.push({ ...metadata, path, method })
-        // could potentially add multiple middlewares to the stack
-        app.use('/', (req, res, n) => {
-          req.metadata = routes.slice(1)
-          n()
-        })
+    app[method] = function (...args) {
+      if (typeof args[1] === 'object' && !Array.isArray(args[1])) {
+        // Option 1: path, metadata, handlers
+        const [path, metadata = {}, ...handlers] = args
+        if (path.startsWith('/')) {
+          routes.push({ method: method.toUpperCase(), path, ...metadata })
+          app.use('/', (req, res, n) => {
+            req.metadata = routes.slice(1)
+            n()
+          })
+        }
+        return original.call(app, path, ...handlers)
+      } else {
+        // Option 2: path, handlers (no metadata)
+        const [path, ...handlers] = args
+        if (path.startsWith('/')) {
+          routes.push({ method: method.toUpperCase(), path })
+          app.use('/', (req, res, n) => {
+            req.metadata = routes.slice(1)
+            n()
+          })
+        }
+        return original.call(app, path, ...handlers)
       }
-      return original.apply(app, [path, ...handlers])
     }
   })
 
   // root route for listing endpoints
-  app.get('/', {}, (req, res) => {
+  app.get('/', (req, res) => {
     // route (optional: serves index.html automatically)
     res.render('index', {
       title: options ? options.title : 'My API Documentation',
