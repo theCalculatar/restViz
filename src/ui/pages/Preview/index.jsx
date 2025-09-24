d1c48782243d523ecee58372b7b38f93846c0b00
theCalculatar <alpheusm13@gmail.com>
2025-09-24 23:21:47 +0200

    to use later.

--- src/ui/pages/Preview/index.jsx
+++ src/ui/pages/Preview/index.jsx
@@ -1,19 +1,14 @@
 // import React from 'react'
 
-import {
-  Badge,
-  Box,
-  Button,
-  Card,
-  Container,
-  Flex,
-  Section,
-} from '@radix-ui/themes'
+import { Container, Flex, Section } from '@radix-ui/themes'
 import SandBox from '../../components/SandBox'
 import RouteDescrption from '../../components/RouteDescrption'
-import { Copy } from 'lucide-react'
+import { useContext } from 'preact/hooks'
+import { AppContext } from '../../context'
 
 function Preview() {
+  const { routes } = useContext(AppContext)
+  
   return (
     <Section p={'3'} height={'100%'} width={'100%'} overflow={'auto'}>
       <Container size={{ md: '3', xl: '4', sm: '3', initial: '1' }}>
