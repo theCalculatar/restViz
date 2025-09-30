dd3336e1304d45f888117cad90e0fe00def235df
theCalculatar <alpheusm13@gmail.com>
2025-09-30 20:03:40 +0200

    feat: implement route handling in Preview component

--- src/ui/pages/Preview/index.jsx
+++ src/ui/pages/Preview/index.jsx
@@ -3,12 +3,27 @@
 import { Container, Flex, Section } from '@radix-ui/themes'
 import SandBox from '../../components/SandBox'
 import RouteDescrption from '../../components/RouteDescrption'
-import { useContext } from 'preact/hooks'
+import { useContext, useEffect } from 'preact/hooks'
 import { AppContext } from '../../context'
+import { useLocation, useNavigate } from 'react-router-dom'
+import { act_setCurrentRoute } from '../../context/actions'
 
 function Preview() {
-  const { routes } = useContext(AppContext)
-  
+  const { routes, activeRoute, setCurrentRoute } = useContext(AppContext)
+  const { pathname } = useLocation()
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (routes.length > 0) {
+      const route = routes.find((r) => r.path == pathname.replace('/test', ''))
+      if (route) {
+        setCurrentRoute({ type: act_setCurrentRoute, payload: route })
+      } else {
+        navigate('/404')
+      }
+    }
+  }, [pathname])
+
   return (
     <Section p={'3'} height={'100%'} width={'100%'} overflow={'auto'}>
       <Container size={{ md: '3', xl: '4', sm: '3', initial: '1' }}>
