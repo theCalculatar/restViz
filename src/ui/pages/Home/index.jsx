7fb512de5bba47058f94e3adee195c86e008cd39
theCalculatar <alpheusm13@gmail.com>
2025-10-12 13:14:10 +0200

    feat: add Command component for enhanced user command input and navigation

--- src/ui/pages/Home/index.jsx
+++ src/ui/pages/Home/index.jsx
@@ -6,29 +6,58 @@ import {
   Container,
   Flex,
   Heading,
-  Progress,
   Section,
   Text,
   TextField,
 } from '@radix-ui/themes'
-import { useContext } from 'preact/hooks'
+import { useContext, useEffect, useMemo, useState } from 'preact/hooks'
 import { AppContext } from '../../context'
 import {
   Activity,
-  BookOpen,
   CheckCircle,
   Code,
   ExternalLink,
   Globe,
   Group,
-  Search,
   Users,
-  Zap,
+  X,
 } from 'lucide-react'
 import { QuickActions } from '../../components/QuickActions'
+import { groupRoutesFn } from '../../utils/routesUtils'
+import { Link } from 'react-router-dom'
+import { Command } from '../../utils/command'
 
 export function Home() {
-  const { config } = useContext(AppContext)
+  const { config, routes, history } = useContext(AppContext)
+  const [isOperational, setisOperational] = useState('Down')
+  const totalEndpoins = useMemo(() => routes.length, [])
+  const endpointGroups = useMemo(() => groupRoutesFn(routes).size, [])
+  const requestsToday = useMemo(() => {
+    return history.filter((req) => {
+      const today = new Date()
+      const reqDate = new Date(req.timestamp)
+      return (
+        reqDate.getDate() === today.getDate() &&
+        reqDate.getMonth() === today.getMonth() &&
+        reqDate.getFullYear() === today.getFullYear()
+      )
+    }).length
+  }, [history])
+
+  useEffect(() => {
+    // Simulate fetching API status from base endpoint
+    const fetchApiStatus = async () => {
+      try {
+        const response = await fetch(config.base)
+        const data = response.status
+        if (data === 200) setisOperational('Operational')
+      } catch (error) {
+        setisOperational('Down')
+      }
+    }
+
+    fetchApiStatus()
+  })
 
   return (
     <Section
@@ -51,12 +80,7 @@ export function Home() {
         </Box>
 
         <Flex gap={'2'} mt={'6'} justify={'center'} align={'center'}>
-          <TextField.Root radius="large" placeholder="Search the docs...">
-            <TextField.Slot>
-              <Search />
-            </TextField.Slot>
-          </TextField.Root>
-          <Button radius="large">Search</Button>
+          <Command />
         </Flex>
 
         {/* API STATUS & METRICS */}
@@ -71,7 +95,12 @@ export function Home() {
               <Flex direction={'column'}>
                 <Text size={'2'}>API status</Text>
                 <Text weight={'bold'} className={'flex items-center gap-1'}>
-                  <CheckCircle className={'text-green-500'} /> operational
+                  {isOperational !== 'Down' ? (
+                    <CheckCircle className={'text-green-500'} />
+                  ) : (
+                    <X className={'text-red-500'} />
+                  )}
+                  {isOperational}
                 </Text>
               </Flex>
               <Activity className={'text-green-500'} />
@@ -86,7 +115,7 @@ export function Home() {
                   size={'4'}
                   className={'flex items-center gap-1'}
                 >
-                  10
+                  {totalEndpoins}
                 </Text>
               </Flex>
               <Globe className={'text-blue-500'} />
@@ -101,7 +130,7 @@ export function Home() {
                   size={'4'}
                   className={'flex items-center gap-1'}
                 >
-                  3
+                  {endpointGroups}
                 </Text>
               </Flex>
               <Group className={'text-purple-500'} />
@@ -110,13 +139,13 @@ export function Home() {
           <Card className={'w-full'}>
             <Flex gap={'4'} align={'center'} justify={'between'}>
               <Flex direction={'column'}>
-                <Text size={'2'}>API status</Text>
+                <Text size={'2'}>Request today</Text>
                 <Text
                   weight={'bold'}
                   size={'4'}
                   className={'flex items-center gap-1'}
                 >
-                  1
+                  {requestsToday}
                 </Text>
               </Flex>
               <Activity className={'text-orange-500'} />
@@ -135,40 +164,60 @@ export function Home() {
               Helpful resources and external links
             </Text>
           </Flex>
-          <Flex gap={'2'} direction={{ initial: 'column', md: 'row' }} mt={'4'}>
-            <Card className={'w-full'} size={'1'} variant="surface">
-              <Flex gap={'2'} align={'center'}>
-                <Code />
-                <Flex direction={'column'}>
-                  <Text size={'2'}>SDK Documentation</Text>
-                  <Text size={'1'} color="gray">
-                    Client libraries and examples
-                  </Text>
+          <Flex gap={'2'} direction={{ initial: 'column', sm: 'row' }} mt={'4'}>
+            <Link
+              className={'w-full'}
+              to={'https://restviz.vercel.app/docs'}
+              target="_blank"
+            >
+              <Card size={'1'} variant="surface">
+                <Flex gap={'2'} align={'center'}>
+                  <Code />
+                  <Flex direction={'column'}>
+                    <Text size={'2'}>SDK Documentation</Text>
+                    <Text size={'1'} color="gray">
+                      Client libraries and examples
+                    </Text>
+                  </Flex>
                 </Flex>
-              </Flex>
-            </Card>
-            <Card className={'w-full'} size={'1'} variant="surface">
-              <Flex gap={'2'} align={'center'}>
-                <ExternalLink />
-                <Flex direction={'column'}>
-                  <Text size={'2'}>Contribute</Text>
-                  <Text size={'1'} color="gray">
-                    Improve the docs or report issues
-                  </Text>
+              </Card>
+            </Link>
+
+            <Link
+              to={'https://github.com/theCalculatar/restViz/issues'}
+              target="_blank"
+              className={'w-full'}
+            >
+              <Card size={'1'} variant="surface">
+                <Flex gap={'2'} align={'center'}>
+                  <ExternalLink />
+                  <Flex direction={'column'}>
+                    <Text size={'2'}>Contribute</Text>
+                    <Text size={'1'} color="gray">
+                      Improve the docs or report issues
+                    </Text>
+                  </Flex>
                 </Flex>
-              </Flex>
-            </Card>
-            <Card className={'w-full'} size={'1'} variant="surface">
-              <Flex gap={'2'} align={'center'}>
-                <Users />
-                <Flex direction={'column'}>
-                  <Text size={'2'}>Developer support</Text>
-                  <Text size={'1'} color="gray">
-                    Get help from the community
-                  </Text>
+              </Card>
+            </Link>
+
+            <Link
+              to="https://github.com/theCalculatar/restViz/issues"
+              target="_blank"
+              className={'w-full'}
+            >
+              <Card size={'1'} variant="surface">
+                <Flex gap={'2'} align={'center'}>
+                  <Users />
+                  <Flex direction={'column'}>
+                    <Text size={'2'}>Developer support</Text>
+                    <Text size={'1'} color="gray">
+                      Get help from the community
+                    </Text>
+                  </Flex>
                 </Flex>
-              </Flex>
-            </Card>
+              </Card>
+            </Link>
           </Flex>
         </Card>
       </Container>
