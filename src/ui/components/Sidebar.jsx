3a72fa07b0d6fbe6f635500fd420c5678660e324
theCalculatar <alpheusm13@gmail.com>
2025-10-17 18:32:31 +0200

    style: polish UI add icons

--- src/ui/components/Sidebar.jsx
+++ src/ui/components/Sidebar.jsx
@@ -18,7 +18,16 @@ import {
   act_setCurrentRoute,
   act_toogleNav,
 } from '../context/actions'
-import { ChevronDown, ChevronUp, PanelLeft } from 'lucide-react'
+import {
+  BookOpen,
+  ChevronDown,
+  ChevronRight,
+  ChevronUp,
+  Folder,
+  PanelLeft,
+  Settings,
+  TestTube2Icon,
+} from 'lucide-react'
 import { methodColors } from '../utils/colors'
 import { useIsMobile } from '../hooks/useMobile'
 
@@ -36,13 +45,13 @@ function Sidebar() {
 
   const isMobile = useIsMobile()
   const navigate = useNavigate()
-  const location = useLocation()
+  const { pathname } = useLocation()
 
   useEffect(() => {
-    if (!location.pathname.includes('test')) {
+    if (!pathname.includes('test')) {
       toogleNav(act_closeNav)
     }
-  }, [location])
+  }, [pathname])
 
   return (
     <aside
@@ -97,13 +106,16 @@ function Sidebar() {
                     }}
                   >
                     <Flex justify={'between'} align={'center'}>
-                      <Text size={'2'} className="IconButton">
-                        {group[0]}
-                      </Text>
-                      <Box>
-                        <ChevronUp className={'ico-closed'} />
-                        <ChevronDown className={'ico-open'} />
-                      </Box>
+                      <Flex align={'center'} gap={'2'}>
+                        <Box>
+                          <ChevronRight className={'ico-closed'} />
+                          <ChevronDown className={'ico-open'} />
+                        </Box>
+                        <Folder className={'text-blue-500'} />
+                        <Text size={'2'} className="IconButton">
+                          {group[0]}
+                        </Text>
+                      </Flex>
                     </Flex>
                   </Box>
                 </Collapsible.Trigger>
@@ -118,7 +130,7 @@ function Sidebar() {
                           direction={'column'}
                           width={'100%'}
                           className={`w-full dark:hover:bg-white/10 hover:bg-black/5 ${
-                            route.url === activeRoute.url &&
+                            '/test/' + route.url === pathname &&
                             'bg-black/10 dark:bg-white/15'
                           }`}
                           style={{
@@ -160,15 +172,43 @@ function Sidebar() {
       </ScrollArea>
       <Flex
         direction={'column'}
-        className={'w-full border-t dark:border-white/10 border-black/10 p-2'}
+        className={'w-full border-t dark:border-white/10 border-black/10 py-2'}
       >
-        <Link to={'/settings'} className={'w-full'}>
-          <Text size={'2'} className={'w-full'}>
-            Settings
-          </Text>
+        <Link
+          to={'/test'}
+          className={`w-full py-1 px-2 rounded-md dark:hover:bg-white/10 hover:bg-black/5 ${
+            pathname === '/test' && 'bg-black/10 dark:bg-white/15'
+          }`}
+        >
+          <Flex gap={'2'} align={'center'}>
+            <TestTube2Icon />
+            <Text size={'2'} className={'w-full'}>
+              Test suits
+            </Text>
+          </Flex>
         </Link>
-        <Link to={'/docs'}>
-          <Text size={'2'}>Documentation</Text>
+        <Link
+          to={'/settings'}
+          className={`w-full py-1 px-2 rounded-md dark:hover:bg-white/10 hover:bg-black/5 ${
+            pathname === '/settings' && 'bg-black/10 dark:bg-white/15'
+          }`}
+        >
+          <Flex gap={'2'} align={'center'}>
+            <Settings />
+            <Text size={'2'} className={'w-full'}>
+              Settings
+            </Text>
+          </Flex>
+        </Link>
+        <Link
+          to={'/docs'}
+          target={'_blank'}
+          className={`py-1 px-2 rounded-md dark:hover:bg-white/10 hover:bg-black/5`}
+        >
+          <Flex gap={'2'} align={'center'}>
+            <BookOpen />
+            <Text size={'2'}>Documentation</Text>
+          </Flex>
         </Link>
       </Flex>
     </aside>
